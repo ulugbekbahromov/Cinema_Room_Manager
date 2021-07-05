eb0fa4d6d817251e0e70ef10fac7dbfec0644d1a
Ulugbek Bakhrom <ulugxodja@gmail.com>
2021-07-05 18:32:14 +0500

    Initial commit

---
+++ Cinema Room Manager/task/src/cinema/Cinema.java
@@ -0,0 +1,13 @@
+package cinema;
+
+public class Cinema {
+
+    public static void main(String[] args) {
+
+        System.out.println("Cinema:\n" +
+                "  1 2 3 4 5 6 7 8");
+        for (int i = 1; i <= 7; i++) {
+            System.out.println(i + " S S S S S S S S");
+        }
+    }
+}
